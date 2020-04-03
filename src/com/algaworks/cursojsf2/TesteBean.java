115c29fed0b678de0c2f0ccc2549f3821f528a83
eduardotrandafilov <eduardo.trandafilov@gmail.com>
2020-04-03 08:06:42 -0300

    Criação projeto BackingBeans

---
+++ src/com/algaworks/cursojsf2/TesteBean.java
@@ -0,0 +1,43 @@
+package com.algaworks.cursojsf2;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlInputText;
+
+@ManagedBean
+public class TesteBean {
+	private HtmlInputText inputText ;
+	private HtmlCommandButton commandButton;
+	
+	public void testar(){
+		
+		if (this.inputText.isDisabled()){
+			
+			this.inputText.setDisabled(false);
+			this.commandButton.setStyle("");
+
+		}else{
+			
+			this.inputText.setDisabled(true);
+			this.commandButton.setStyle("background-color:red;color:white");
+	
+		}
+
+	}
+	
+	public HtmlInputText getInputText() {
+		return inputText;
+	}
+	public void setInputText(HtmlInputText inputText) {
+		this.inputText = inputText;
+	}
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+	
+	
+	
+}
